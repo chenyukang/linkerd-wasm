6eb8c313c63c3e9893da1d62b9a39c52040787da
yukang <moorekang@gmail.com>
2021-08-19 13:09:46 +0000

    refactor k8s conf

--- image-api-rs/run_api_rs.sh
+++ image-api-rs/run_api_rs.sh
@@ -1,7 +1,1 @@
-dapr run --app-id image-api-rs \
-         --app-protocol http \
-         --app-port 9004 \
-         --dapr-http-port 3502 \
-         --components-path ../config \
-         --log-level debug \
-         ./target/debug/image-api-rs
+kubectl apply -f pod.yml
